7df4771bd7b390a3313386af6b4bb2e61e42c805
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-03-28 17:05:31 +0300

    added the function of updating user's avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const express = require("express");
 const usersController = require("../../controllers/usersController");
 const { registerSchema, loginSchema } = require("../../schemas/users");
 const auth = require("../../middleware/auth");
+const handleAvatar = require("../../middleware/handleAvatar");
 const validateBody = require("../../middleware/validateData");
 
 const router = express.Router();
@@ -15,5 +16,11 @@ router.post(
 router.post("/login", validateBody(loginSchema), usersController.login);
 router.post("/logout", auth, usersController.logout);
 router.post("/current", auth, usersController.getCurrent);
+router.patch(
+  "/avatars",
+  auth,
+  handleAvatar.single("avatar"),
+  usersController.updateAvatar
+);
 
 module.exports = router;
